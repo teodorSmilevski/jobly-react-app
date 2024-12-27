6ecb4f75c4d5012ad2bad9eee0d62aa7930856ae
teodorSmilevski <teosmilevski@outlook.com>
2024-12-27 02:34:52 +0100

    Added edit job functionality

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import JobsPage from "./pages/JobsPage";
 import NotFound from "./pages/NotFound";
 import JobPage, { jobLoader } from "./pages/JobPage";
 import AddJobPage from "./pages/AddJobPage";
+import EditJobPage from "./pages/EditJobPage";
 
 export default function App() {
   const addJob = async (newJob) => {
@@ -24,6 +25,20 @@ export default function App() {
     if (!res.ok) throw new Error("Failed to add job");
 
     return res.json();
+  };
+
+  const updateJob = async (job) => {
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "applicaiton/json",
+      },
+      body: JSON.stringify(job),
+    });
+
+    if (!res.ok) throw new Error("Failed to update job");
+
+    return;
   };
 
   const deleteJob = async (id) => {
@@ -43,6 +58,11 @@ export default function App() {
         <Route
           path="/jobs/:id"
           element={<JobPage deleteJob={deleteJob} />}
+          loader={jobLoader}
+        />
+        <Route
+          path="/edit-job/:id"
+          element={<EditJobPage updateJobSubmit={updateJob} />}
           loader={jobLoader}
         />
         <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
